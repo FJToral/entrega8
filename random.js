bd2da91e12e5beb026905e52997ffdbfe8828e7c
Francisco Javier Toral <franciscojavier.toral.zamorano@alumnos.upm.es>
2015-04-22 21:24:49 +0200

    random version 1

---
+++ random.js
@@ -0,0 +1,11 @@
+//Parte 4, entrega 2.2: Elena Lourés Piñeiro 53486374N y Francisco Javier Toral Zamorano 53842459B 
+
+// Math.random() devuelve número aleatorio entre 0 y 1.
+var numero = Math.random();
+
+if(numero <= 0.0){
+	console.log("\n" + numero + " MENOR que 0.5 \n");
+}
+else {
+	console.log("\n" + numero + " MAYOR que 0.5 \n");
+}
